49bc3d439c1cd2ba6d18844deec39d8686e71b4f
maty00905 <oropezamatias52@gmail.com>
2026-04-21 11:15:56 -0300

    Change alert message on form submission
    Updated alert message for form submission.

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+// DARK MODE
+const btn = document.getElementById("toggleTheme");
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("light");
+});
+
+// FORM
 document.getElementById("form").addEventListener("submit", function(e) {
     e.preventDefault();
-    alert("Mensaje enviado correctamente");
+    alert("Mensaje enviado");
 });
+
+// FADE ANIMATION
+const faders = document.querySelectorAll(".fade");
+
+const appear = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+faders.forEach(el => appear.observe(el));
